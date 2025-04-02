d097987471f6932d720163345485450dd9c56a43
Ashish Shrestha <ashishshr27@gmail.com>
2025-04-02 20:29:51 +0545

    post method

---
+++ post_welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="global.css">
+</head>
+
+<body>
+  <!-- Receive the form details using `POST` and display the fullname and email -->
+</body>
+
+</html>
