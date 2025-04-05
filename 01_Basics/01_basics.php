7fb7e8a619e8ebfe74eb984e7837f0957e5cf2b6
Ashish Shrestha <ashishshr27@gmail.com>
2025-04-05 09:53:56 +0545

    basics

---
+++ 01_Basics/01_basics.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>01_basics</title>
+  <link rel="stylesheet" href="../styles/global.css">
+</head>
+
+<body>
+  <section>
+    <!-- 1. Insert PHP code to print your faculty using print() -->
+  </section>
+
+  <section>
+    <!-- 2. Insert PHP code to declare a variable named `fullname` and print its value using echo() -->
+  </section>
+
+  <section>
+    <!-- 3. Insert PHP code to print your fullname without any function -->
+  </section>
+
+  <section>
+    <!-- 4. Insert PHP code to print your favorite fruit inside heading -->
+  </section>
+
+  <section>
+    <!-- 5. Insert PHP code to declare a constant value `birthYear` and print its value -->
+  </section>
+
+  <section>
+    <!-- 6. Insert PHP code to insert a `h2` and button -->
+  </section>
+
+</body>
+
+</html>
---
+++ 01_Basics/03_basics.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>03_basics</title>
+  <link rel="stylesheet" href="../styles/global.css">
+</head>
+
+<body>
+  <section>
+    <!-- 1. Insert PHP code to print numbers from 1 to 5 using while loop -->
+  </section>
+
+  <section>
+    <!-- 2. Insert PHP code to print numbers from 5 to 10 using do-while loop -->
+  </section>
+
+  <section>
+    <!-- 3. Insert PHP code to print numbers from 10 to 15 using for loop -->
+  </section>
+
+  <section>
+    <!-- 4. Insert PHP code to create indexed array fruits containing "Apple", "Banana", and "Cherry". Store the first fruit in a variable named `firstFruit` and display it -->
+  </section>
+
+  <section>
+    <!-- 5. Insert PHP code to create an array colors containing "Red", "Green", "Blue". Add "Yellow" to the array using array_push() -->
+  </section>
+
+  <section>
+    <!-- 6. Insert PHP code to create an array containing three names using array() and print the names using foreach() loop -->
+  </section>
+
+  <section>
+    <!-- 7. Insert PHP code to create associative array named `student` and store id, name and gender. Display the id and name of the student -->
+  </section>
+
+  <section>
+    <!-- 8. Insert PHP code to print today's date, day, month and current time -->
+  </section>
+
+  <section>
+    <!-- 9. Insert PHP code to create a function to display your first name` -->
+  </section>
+
+  <section>
+    <!-- 10. Insert PHP code to create a function to pass your first and last name and display the fullname concatenating them -->
+  </section>
+
+  <section>
+    <!-- 11. Insert PHP code to read contents of a file using fread() and display them -->
+  </section>
+
+  <section>
+    <!-- 12. Insert PHP code to write your name using fwrite() to a file -->
+  </section>
+</body>
+
+</html>
